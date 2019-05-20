80625f79ac0466a31f4c5cc3c256328c5649de45
Yilun Weng <wengellen@gmail.com>
2019-05-20 14:11:01 -0700

    debug session started

--- index.js
+++ index.js
@@ -5,10 +5,11 @@
 // 	require('./dist')
 // 	console.log('production!!!!')
 // }
-if(process.env.node_env === "development"){
-	require('nodemon')({script: './dev.js'})
-	console.log('dev!!!!')
-}else{
-	require('./src')
-	console.log('production!!!!')
-}
+// if(process.env.node_env === "development"){
+// 	require('nodemon')({script: './dev.js'})
+// 	console.log('dev!!!!')
+// }else{
+// 	require('./src')
+// 	console.log('production!!!!')
+// }
+require('./src')
