5a5374b6b03b9536c52ac1e245555ce12a5b282e
Yilun Weng <wengellen@gmail.com>
2019-05-20 14:01:20 -0700

    update package.json

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+// if(process.env.node_env === "development"){
+// 	require('nodemon')({script: './dev.js'})
+// 	console.log('dev!!!!')
+// }else{
+// 	require('./dist')
+// 	console.log('production!!!!')
+// }
 if(process.env.node_env === "development"){
 	require('nodemon')({script: './dev.js'})
 	console.log('dev!!!!')
 }else{
-	require('./dist')
+	require('./src')
 	console.log('production!!!!')
 }
